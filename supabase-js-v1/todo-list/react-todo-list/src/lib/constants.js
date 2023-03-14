57d47aa16655fc902e8cb7a1d909bd56be0a74ab
Lucas Jones <lucasejones+1@gmail.com>
2023-03-14 10:05:01 -0700

    Initial Config

--- supabase-js-v1/todo-list/react-todo-list/src/lib/constants.js
+++ supabase-js-v1/todo-list/react-todo-list/src/lib/constants.js
@@ -1,2 +1,5 @@
-export const REACT_APP_SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-export const REACT_APP_SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
+export const REACT_APP_SUPABASE_URL = 'https://vbpnavfhpndevywgpptg.supabase.co'
+export const REACT_APP_SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZicG5hdmZocG5kZXZ5d2dwcHRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg4MTI1NTcsImV4cCI6MTk5NDM4ODU1N30.sjJga-_X-mZG7Wz4Gy1MsHShzalRCY7dHOBBCQ0ETC4'
+
+// export const REACT_APP_SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+// export const REACT_APP_SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
